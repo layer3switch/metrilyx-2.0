749fa821d9bc8e3dca08da6b5ff172d98dc3e24c
abs <abs.pathak@ticketmaster.com>
2014-02-17 19:58:46 -0800

    Fix for not recognizing the absolute path of the file

--- metrilyx/metrilyxconfig.py
+++ metrilyx/metrilyxconfig.py
@@ -1,14 +1,17 @@
 import os 
 from datastores import jsonFromFile
 
-CONFIG_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "etc/metrilyx/metrilyx.conf")
+_abspath = os.path.abspath(__file__)
+
+CONFIG_FILE = os.path.join(os.path.dirname(os.path.dirname(_abspath)), "etc/metrilyx/metrilyx.conf")
 config = jsonFromFile(CONFIG_FILE)
 ## Not exposed to the user.
-config["static_path"] = os.path.join(os.path.dirname(__file__), "static")
-config["schema_path"] = os.path.join(os.path.dirname(os.path.dirname(__file__)), "schemas")
+config["static_path"] = os.path.join(os.path.dirname(_abspath), "static")
+config["schema_path"] = os.path.join(os.path.dirname(os.path.dirname(_abspath)), "schemas")
 
 if not config.has_key("model_path"):
-	config["model_path"] = os.path.join(os.path.dirname(os.path.dirname(__file__)), "pagemodels")
+	config["model_path"] = os.path.join(os.path.dirname(os.path.dirname(_abspath)), "pagemodels")
 
 #from pprint import pprint
+#print CONFIG_FILE
 #pprint(config)
