c7c6c1d28b4f347488fb8029f9408f96ab76da41
abs <abs.pathak@ticketmaster.com>
2014-04-04 18:13:34 -0700

    added comments

--- metrilyx/static/config.js
+++ metrilyx/static/config.js
@@ -1,3 +1,10 @@
+/* 
+ * This is a list of servers to use for backend calls.
+ * This was specifically added to bypass the browser limitation
+ * in the number of ajax requests that can be made.
+ *
+ * It can be a list of cname's to the server as well.
+ */
 CONN_POOL_CFG = {
 	urls: [
 	"http://metdev1.lcl:8000",
