81a4d1fc533bc370d8b05ca8f319e2c1a587a44e
abs <abs.pathak@ticketmaster.com>
2014-04-06 18:07:25 -0700

    Added debian install support

--- install.sh
+++ install.sh
@@ -1,11 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+INSTALL_ROOT="/opt";
+INSTALL_TIME=$(date '+%d%b%Y_%H%M');
+APP_HOME="${INSTALL_ROOT}/metrilyx";
+
+if [[ -f "/etc/redhat-release" ]]; then
+	HTTPD="httpd"
+	HTTP_USER="apache"
+	PKGS="libuuid gcc uuid ${HTTPD} mod_wsgi python-setuptools python-devel mongodb"
+	PKG_INSTALLER="yum -y install"
+	PKG_LISTER="rpm -qa"
+	PKG_S_PREFIX="^"
+elif [[ -f "/etc/debian_version" ]]; then
+	HTTPD="apache2"
+	HTTP_USER="www-data"
+	PKGS="libuuid1 gcc uuid ${HTTPD} libapache2-mod-wsgi python-setuptools python-dev mongodb"
+	PKG_INSTALLER="apt-get install -y"
+	PKG_LISTER="dpkg -l"
+	PKG_S_PREFIX="ii\s+"
+else
+	echo "Currently only RedHat/Debian based distro are supported.  Please install manually.";
+	exit 1;
+fi
+
+clean() {
+	find . -name '*.pyc' -exec rm -rvf '{}' \;
+}
+setup_app_dirs() {
+	mkdir -p ${APP_HOME};
+	cp -a . ${APP_HOME}/;
+	chmod g+w ${APP_HOME};
+	( id celery 2>&1 ) > /dev/null || useradd celery;
+	chgrp celery ${APP_HOME};
+}
+setup_startup_config() {
+	cp -a etc/rc.d/init.d/* /etc/rc.d/init.d/;
+	if [ ! -f /etc/sysconfig/celeryd ]; then 
+		cp etc/sysconfig/celeryd /etc/sysconfig/;
+	fi	
+}
 install_os_deps() {
 	echo "-- Installing OS dependencies...."
-	for pkg in libuuid gcc uuid httpd mod_wsgi python-setuptools python-devel mongodb; do
-		rpm -qa | egrep "^${pkg}" || yum -y install ${pkg};
+	for pkg in ${PKGS}; do
+		$PKG_LISTER | egrep "${PKG_S_PREFIX}${pkg}" || $PKG_INSTALLER ${pkg};
 	done;
-	chkconfig httpd on;
 };
 install_pydeps() {
 	echo "-- Installing python dependencies..."
@@ -18,73 +56,69 @@ install_deps() {
 	install_os_deps;
 	install_pydeps;
 }
-clean() {
-	find . -name '*.pyc' -exec rm -rvf '{}' \;
+backup_curr_install() {
+	if [ -d "${INSTALL_ROOT}/metrilyx" ]; then
+		echo "- Backing up existing installation...";
+		mv ${APP_HOME} ${APP_HOME}-${INSTALL_TIME};
+	fi;
+}
+setup_app_config() {
+	if [ -f "/opt/metrilyx-${INSTALL_TIME}/etc/metrilyx/metrilyx.conf" ]; then
+		echo "- Importing existing data..."
+		echo "  configs...";
+		cp ${APP_HOME}-${INSTALL_TIME}/etc/metrilyx/metrilyx.conf ${APP_HOME}/etc/metrilyx/metrilyx.conf;
+		echo "  dashboards..."
+		cp -a ${APP_HOME}-${INSTALL_TIME}/pagemodels/ ${APP_HOME}/pagemodels/;
+		echo "  heatmap index..."
+		cp -f ${APP_HOME}-${INSTALL_TIME}/heatmaps.json ${APP_HOME}/heatmaps.json;
+		echo "  heatmaps..."
+		cp -a ${APP_HOME}-${INSTALL_TIME}/heatmaps/ ${APP_HOME}/heatmaps/;
+	else
+		cp etc/metrilyx/metrilyx.conf.sample ${APP_HOME}/etc/metrilyx/metrilyx.conf;
+		${EDITOR:-vi} ${APP_HOME}/etc/metrilyx/metrilyx.conf;
+	fi
 }
 install_app(){
 	clean;
 
-	echo "- Stopping apache...";
-	/etc/init.d/httpd stop > /dev/null 2>&1;
+	#echo "- Stopping apache...";
+	#/etc/init.d/${HTTPD} stop > /dev/null 2>&1;
 	
-	install_time=$(date '+%d%b%Y_%H%M');
-	
-	if [ -d /opt/metrilyx ]; then
-		echo "- Backing up existing installation...";
-		mv /opt/metrilyx /opt/metrilyx-${install_time};
-	fi;
+	backup_curr_install;
 	
 	echo "- Installing app..."
-	mkdir -p /opt/metrilyx;
-	cp -a . /opt/metrilyx/;
-	chmod g+w /opt/metrilyx;
-	id celery || useradd celery;
-	chgrp celery /opt/metrilyx;
+	setup_app_dirs;
+	setup_app_config;
 	
-	cp -a etc/rc.d/init.d/* /etc/rc.d/init.d/;
-	if [ ! -f /etc/sysconfig/celeryd ]; then 
-		cp etc/sysconfig/celeryd /etc/sysconfig/;
+}
+app_postinstall() {
+	if [[ -f "/etc/debian_version" ]]; then
+		a2enmod rewrite;
+	elif [[ -f "/etc/redhat-release" ]]; then
+		setup_startup_config;
 	fi
-	if [ -f "/opt/metrilyx-${install_time}/etc/metrilyx/metrilyx.conf" ]; then
-		echo "- Importing existing data..."
-		echo "  configs...";
-		cp /opt/metrilyx-${install_time}/etc/metrilyx/metrilyx.conf /opt/metrilyx/etc/metrilyx/metrilyx.conf;
-		echo "  dashboards..."
-		cp -a /opt/metrilyx-${install_time}/pagemodels/ /opt/metrilyx/pagemodels/;
-		echo "  heatmap index..."
-		cp -f /opt/metrilyx-${install_time}/heatmaps.json /opt/metrilyx/heatmaps.json;
-		echo "  heatmaps..."
-		cp -a /opt/metrilyx-${install_time}/heatmaps/ /opt/metrilyx/heatmaps/;
-	else
-		cp etc/metrilyx/metrilyx.conf.sample /opt/metrilyx/etc/metrilyx/metrilyx.conf;
-		${EDITOR:-vi} /opt/metrilyx/etc/metrilyx/metrilyx.conf;
-	fi
-	
+	#/etc/init.d/${HTTPD} restart;
 }
 install_web_config() {
 	echo "- Installing web components..."
-	cp etc/httpd/conf.d/metrilyx.conf /etc/httpd/conf.d/;
-	chown -R apache:apache /opt/metrilyx;
-	echo "- Restarting apache..."
-	/etc/init.d/httpd restart
+	cp etc/httpd/conf.d/metrilyx.conf /etc/${HTTPD}/conf.d/;
+	chown -R $HTTP_USER ${APP_HOME};
 }
-
 ##### Main ####
 
-if [ ! -f "/etc/redhat-release" ]; then 
-	echo "Currently only RedHat based distro are supported.  Please install manually.";
-	exit 1;
-fi
+
 
 case "$1" in
 	lyx)	
 		install_deps;
 		install_app;
+		app_postinstall;
 		;;
 	all)
 		install_deps;
 		install_app;
 		install_web_config;
+		app_postinstall;
 		;;	
 	*)
 		echo -e "\n\tUsage:\n\t\t$0\t[lyx|all]\n";
