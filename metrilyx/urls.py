656b7c6f9a8a807acd84702500e0b718b81004aa
abs <abs.pathak@ticketmaster.com>
2014-05-05 19:00:08 -0700

    Cleaned up url mappings in config. Added static file serving in dev mode to match deployment mappings.

--- metrilyx/urls.py
+++ metrilyx/urls.py
@@ -1,6 +1,7 @@
 
 import os
 
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
@@ -13,22 +14,24 @@ from metrilyxconfig import config
 from metrilyx import apiviews
 
 router = routers.SimpleRouter(trailing_slash=False)
-router.register(r'api/users', apiviews.UserViewSet)
-router.register(r'api/groups', apiviews.GroupViewSet)
-router.register(r'api/graphmaps', apiviews.GraphMapViewSet)
-router.register(r'api/heatmaps', apiviews.HeatMapViewSet)
-router.register(r'api/schemas', apiviews.SchemaViewSet, base_name="schemas")
-router.register(r'api/search', apiviews.SearchViewSet, base_name="search")
-router.register(r'api/tags', apiviews.TagViewSet, base_name="tags")
+router.register(r'(api/)?users', apiviews.UserViewSet)
+router.register(r'(api/)?groups', apiviews.GroupViewSet)
+router.register(r'(api/)?graphmaps', apiviews.GraphMapViewSet)
+router.register(r'(api/)?heatmaps', apiviews.HeatMapViewSet)
+router.register(r'(api/)?schemas', apiviews.SchemaViewSet, base_name="schemas")
+router.register(r'(api/)?search', apiviews.SearchViewSet, base_name="search")
+router.register(r'(api/)?tags', apiviews.TagViewSet, base_name="tags")
 
 urlpatterns = patterns('',
-    # url(r'^$', apiviews.index),
-    # (api/)* is to allow apache to point to /api and work from CLI
-    # as well as from a webserver
-    url(r'^', include(router.urls)),
-    url(r'^(api/)*graph(/(?P<graph_query>.*)|/*)$', apiviews.GraphView.as_view()),
-    url(r'^(api/)*heat(/(?P<heat_id>.*)|/*)$', apiviews.HeatView.as_view()),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/?', include(admin.site.urls)),
+	# url(r'^$', apiviews.index),
+	# (api/)* is to allow apache to point to /api and work from CLI
+	# as well as from a webserver
+	url(r'^', include(router.urls)),
+	url(r'^(api/)?graph(/(?P<graph_query>.*)|/*)$', apiviews.GraphView.as_view()),
+	url(r'^(api/)?heat(/(?P<heat_id>.*)|/*)$', apiviews.HeatView.as_view()),
+	url(r'^(api/)?api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	url(r'^(api/)?admin/?', include(admin.site.urls)),
 )
-urlpatterns += static('/', document_root=os.path.join(os.path.dirname(__file__), 'static'))    
+if config['debug']:
+	urlpatterns += static('/', 
+			document_root=os.path.join(os.path.dirname(__file__), 'static'))   
