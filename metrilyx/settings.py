6b251d4c0ef45145aebb67d80eeac9455742713e
abs <abs.pathak@ticketmaster.com>
2014-05-07 11:06:48 -0700

    Disabled auth.  With this enabled, the browser will show an auth window before PUT, POST and DELETE operations.

--- metrilyx/settings.py
+++ metrilyx/settings.py
@@ -117,10 +117,10 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     # This will activate auth on the api endpoints.
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
+    #'DEFAULT_AUTHENTICATION_CLASSES': (
+    #    'rest_framework.authentication.BasicAuthentication',
+    #    'rest_framework.authentication.SessionAuthentication',
+    #)
 }
 INSTALLED_APPS = (
     'django.contrib.auth',
