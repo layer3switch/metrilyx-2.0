519014baf735314eef45be9d20a70399750a989a
abs <abs.pathak@ticketmaster.com>
2014-05-07 20:54:37 -0700

    Code cleanup

--- metrilyx/static/libs/angular-heatmap.js
+++ metrilyx/static/libs/angular-heatmap.js
@@ -7,22 +7,31 @@ angular.module('heatmaps', [])
 				if(!ctrl) return;
 
 				var heatmapInterval = setInterval(function() {
-					if(ctrl.$modelValue && ctrl.$modelValue.series.length > 0) {
-						Heat.getData(ctrl.$modelValue.series[0].query, function(result) {
-							// check for errors
-							ctrl.$modelValue.series[0].data = assignSeverity(result['data'], ctrl.$modelValue.thresholds);
-						});	
+					if(scope.editMode === "") {
+						if(ctrl.$modelValue && ctrl.$modelValue.series.length > 0) {
+							Heat.getData(ctrl.$modelValue.series[0].query, function(result) {
+								// check for errors
+								ctrl.$modelValue.series[0].data = assignSeverity(result['data'], ctrl.$modelValue.thresholds);
+							});	
+						}
 					}
 				}, 60000);
+				
 				scope.$watch(function() {
 					return ctrl.$modelValue;
 				}, function(newValue, oldValue) {
+					if(newValue.series !== undefined) return;
 					if(newValue.series.length < 1) return;
-					Heat.getData(newValue.series[0].query,function(result) {
-						// check for errors
-						newValue.series[0].data = assignSeverity(result['data'], newValue.thresholds);
-					});
+					Heat.getData(newValue.series[0].query,
+						function(result) {
+							// check for errors//
+							newValue.series[0].data = assignSeverity(result['data'], newValue.thresholds);
+						},
+						function(arg1, arg2){
+							console.log(arg1, arg2);
+						});
 				}, true);
+				
 				scope.$on("$destroy", function( event ) {
                 	clearInterval(heatmapInterval);
                 });
