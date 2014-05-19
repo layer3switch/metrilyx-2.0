c6ca2b83063f691a1e68fdaf33431518a3bc9d4d
abs <abs.pathak@ticketmaster.com>
2014-05-19 14:17:23 -0700

    Updated configuration options.

--- metrilyx/metrilyxconfig.py
+++ metrilyx/metrilyxconfig.py
@@ -6,19 +6,6 @@ _apphome = os.path.dirname(os.path.dirname(_abspath))
 
 CONFIG_FILE = os.path.join(_apphome, "etc/metrilyx/metrilyx.conf")
 config = jsonFromFile(CONFIG_FILE)
-## Not exposed to the user.
+
 config["static_path"] = os.path.join(os.path.dirname(_abspath), "static")
 config["schema_path"] = os.path.join(_apphome, "schemas")
-
-if not config.has_key("model_path"):
-	config["model_path"] = os.path.join(_apphome, "pagemodels")
-
-if not config['heatmaps'].has_key("store_path"):
-	config["heatmaps"]["store_path"] = os.path.join(_apphome, "heatmaps")
-
-if not config['heatmaps'].has_key('db_path'):
-	config['heatmaps']['db_path'] = os.path.join(_apphome, "heatmaps.json")
-
-#from pprint import pprint
-#print CONFIG_FILE
-#pprint(config)
