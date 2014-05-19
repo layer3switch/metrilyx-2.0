72bf1f89909d0fa0b5d1f371d62f82f9bdcad274
abs <abs.pathak@ticketmaster.com>
2014-05-18 23:41:15 -0700

    Code cleanup

--- metrilyx-dataserver.py
+++ metrilyx-dataserver.py
@@ -3,61 +3,100 @@
 import sys
 import json
 import time
-from pprint import pprint
+import logging
+
+import multiprocessing
+from optparse import OptionParser
 
 from twisted.internet import reactor
 from twisted.python import log
-#from twisted.web.server import Site
-#from twisted.web.static import File
 
 from autobahn.twisted.websocket import WebSocketServerFactory, listenWS
 
 from metrilyx.metrilyxconfig import config
-
 from metrilyx.dataserver.protocols import GraphServerProtocol, acceptedCompression
 from metrilyx.dataserver.dataproviders import TSDBDataProvider
 
-WS_ADDRESS = "ws://localhost:9000"
 
-"""
-{
-	"start": start_time,
-	"graphType": "line",
-	"name": metric,
-	"series": [{
-		"alias": metric,
-		"yTransform": "",
-		"query": {
-			"metric": metric,
-			"aggregator": "sum",
-			"rate": rate,
-			"tags": {}
-		}
-	}]
-}
-"""
+LOG_FORMAT = "%(asctime)s [%(levelname)s %(name)s]: %(message)s"
+
 class TSDBGraphServerProtocol(GraphServerProtocol):
 	dataprovider = TSDBDataProvider(config['dataproviders']['tsdb'])
 
 
-if __name__ == '__main__':
-
-	if len(sys.argv) > 1 and sys.argv[1] == 'debug':
-		log.startLogging(sys.stdout)
-		debug = True
+def spawn_websocket_server(uri, logLevel, externalPort=None):
+	if logLevel == "DEBUG":
+		isDebug = True
 	else:
-		debug = False
+		isDebug = False
 
-	factory = WebSocketServerFactory(WS_ADDRESS, debug=debug, debugCodePaths=debug)
+	if externalPort == None:
+		factory = WebSocketServerFactory(uri, debug=isDebug)
+	else:
+		factory = WebSocketServerFactory(uri, debug=isDebug, 
+										externalPort=externalPort)
+
 	factory.protocol = TSDBGraphServerProtocol
-	factory.setProtocolOptions(perMessageCompressionAccept=acceptedCompression)
-	listenWS(factory)
-
-	##  service static files
-	#webdir = File(".")
-	#web = Site(webdir)
-	#reactor.listenTCP(8080, web)
+	factory.setProtocolOptions(
+			perMessageCompressionAccept=acceptedCompression)
 	
-	print "Starting websocket server: %s" %(WS_ADDRESS)
+	listenWS(factory)
 	reactor.run()
 
+
+if __name__ == '__main__':
+
+	parser = OptionParser()
+	parser.add_option("-l", "--log-level", dest="logLevel", default="INFO", 
+		help="Logging level.")
+	parser.add_option("-u", "--uri", dest="uri", default="ws://localhost",
+		help="ws://<hostname>")
+	parser.add_option("-s", "--start-port", dest="startPort", type="int", default=9000,
+		help="Starting point of the port range to listen on. This is only applicable when multiple servers are launched.")
+	parser.add_option("-c","--server-count", dest="serverCount", type="int", default=1,
+		help="Number of servers to spawn. If 0 is specified, the count will be based off of the number cpus/cores")
+	parser.add_option("-e", "--external-port", dest="externalPort", type="int", default=None,
+		help="External port to use.  This is needed when running the servers behind a reverse proxy (i.e nginx)")
+
+	(opts, args) = parser.parse_args()
+
+	if not opts.uri:
+		print " --uri required!"
+		parser.print_help()
+		sys.exit(1)
+
+	if opts.logLevel == "DEBUG":
+		# twisted logger (may not be needed)
+		log.startLogging(sys.stdout)
+		observer = log.PythonLoggingObserver()
+		observer.start()
+
+	try:
+		logging.basicConfig(level=eval("logging.%s" %(opts.logLevel)),
+			format=LOG_FORMAT)
+		logger = logging.getLogger(__name__)
+	except Exception,e:
+		print "[ERROR] %s" %(str(e))
+		parser.print_help()
+		sys.exit(2)
+	
+	if opts.serverCount == 0:
+		logger.info("Using auto-spawn count.")
+		opts.serverCount = multiprocessing.cpu_count()
+	logger.info("Spawning %d server/s..." %(opts.serverCount))
+	
+	procs = []
+	for i in range(opts.serverCount):
+		uri = "%s:%d" %(opts.uri, opts.startPort+i)
+		proc = multiprocessing.Process(
+								target=spawn_websocket_server, 
+								args=(uri, opts.logLevel, opts.externalPort))
+		proc.start()
+		logger.info("Started server - %s" %(uri))
+		procs.append(proc)
+
+	try:
+		for p in procs:
+			p.join()
+	except KeyboardInterrupt:
+		logger.info("Stopping...")
