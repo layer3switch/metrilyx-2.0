1c9dff3a8a6f6d0e1c43db76af7902fff8c00e6d
abs <abs.pathak@ticketmaster.com>
2014-06-04 00:55:01 -0700

    initial 2.3 commit

--- metrilyx/static/controllers.js
+++ metrilyx/static/controllers.js
@@ -143,6 +143,7 @@ metrilyxControllers.controller('pageController', ['$scope', '$route', '$routePar
 		$scope.podHtml 			= connectionPool.nextConnection()+"/partials/pod.html";
 		$scope.pageHeaderHtml 	= connectionPool.nextConnection()+"/partials/page-header.html";
 		$scope.jsonHtml 		= connectionPool.nextConnection()+"/partials/json.html";
+		$scope.graphControlsHtml= connectionPool.nextConnection()+"/partials/graph-controls.html";
 
 		$scope.metricListSortOpts 	= dndconfig.metricList;
 		$scope.graphSortOpts 		= dndconfig.graph;
