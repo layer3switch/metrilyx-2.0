c0e95314c5571402c0fc655f309f4d33e18eb66f
abs <abs.pathak@ticketmaster.com>
2014-10-19 22:45:01 -0700

    Updated config changes

--- metrilyx/rest/apiviews.py
+++ metrilyx/rest/apiviews.py
@@ -15,8 +15,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, permissions, filters
 
-from celery import Celery
-
 import metrilyx
 
 from custompermissions import IsGroupOrReadOnly, IsCreatorOrReadOnly
